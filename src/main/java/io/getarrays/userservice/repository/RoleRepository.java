75ff124b6222589290d3ec56ba2249cc497b3987
Raul Castillo <rauln85@gmail.com>
2021-07-25 14:03:06 -0500

    Update name Rol to Role

--- src/main/java/io/getarrays/userservice/repository/RoleRepository.java
+++ src/main/java/io/getarrays/userservice/repository/RoleRepository.java
@@ -3,7 +3,7 @@ package io.getarrays.userservice.repository;
 import io.getarrays.userservice.domain.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RolRepository extends JpaRepository<Role, Long> {
+public interface RoleRepository extends JpaRepository<Role, Long> {
 
     Role findByName(String name);
 }
