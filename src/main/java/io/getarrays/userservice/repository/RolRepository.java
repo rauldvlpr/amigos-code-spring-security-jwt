5d59599288975f78b0f08d14c00f93491b0f8999
Raul Castillo <rauln85@gmail.com>
2021-07-25 13:53:24 -0500

    Add Repositorys

---
+++ src/main/java/io/getarrays/userservice/repository/RolRepository.java
@@ -0,0 +1,9 @@
+package io.getarrays.userservice.repository;
+
+import io.getarrays.userservice.domain.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RolRepository extends JpaRepository<Role, Long> {
+
+    Role findByName(String name);
+}
---
+++ src/main/java/io/getarrays/userservice/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package io.getarrays.userservice.repository;
+
+import io.getarrays.userservice.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User fingByUsername(String username);
+}
